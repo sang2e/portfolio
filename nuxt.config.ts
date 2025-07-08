cc03b829aec123bcddc3b6b042a1fe0c4c716633
박상이 <sang2e6725@gmail.com>
2025-07-08 12:31:44 +0900

    백번째삭제

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,8 @@ export default defineNuxtConfig({
   // ssr: false,
   app: {
     // baseURL: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
-    baseURL: '/portfolio/',
+    cdnURL: '/portfolio/',  // ✅ 반드시 필요함
+    baseURL: '/portfolio/', // 있으면 더 좋고, 없어도 cdnURL만 있어도 됨
   },
   generate: {
     fallback: true, // ✅ 404.html 생성 (SPA 라우팅 대응)
